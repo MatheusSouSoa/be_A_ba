aa44ed574d2ab34c5e271f20a44a48e30e50897a
Matheus Soares <matheus.sousoa.dev@gmail.com>
2023-09-12 09:55:46 -0300

    Delete be-a-ba front end directory

--- be-a-ba front end/src/components/header/header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-    return (
-        <div>
-            <header className="flex bg-green-800 h-24">
-                
-            </header>
-        </div>
-    )
-}
