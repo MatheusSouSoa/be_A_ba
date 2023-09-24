805d3d111275d047381a8fc459fa9f3899792caf
Matheus Souza Soares <980189@queroquero.local>
2023-09-24 13:59:18 -0300

    adição função de editar coluna do template

--- front-end/src/components/content/Templates/cadastrarTemplate/CadastrarTemplate.tsx
+++ front-end/src/components/content/Templates/cadastrarTemplate/CadastrarTemplate.tsx
@@ -5,11 +5,11 @@ const tiposDados = ["texto", "data", "inteiro", "decimal", "booleano", "moeda"];
 const tipoArquivos = ["csv", "xls", "xlsx"];
 
 interface TemplateProps {
-    nome: string;
-    numero_colunas: number;
-    tipo_arquivo: string;
-    limite_linhas?: number | null;
-    colunas: ColunasProps[]
+  nome: string;
+  numero_colunas: number;
+  tipo_arquivo: string;
+  limite_linhas?: number | null;
+  colunas: ColunasProps[];
 }
 interface ColunasProps {
   nome_coluna: string;
@@ -18,14 +18,13 @@ interface ColunasProps {
 }
 
 export default function CreateTemplate() {
+  const templateNome = localStorage.getItem("NomeTemplate") || "";
 
-  const templateNome = localStorage.getItem("NomeTemplate") || "";    
-  
   useEffect(() => {
-    setNomeTemplate(templateNome)
-  }, [])
+    setNomeTemplate(templateNome);
+  }, []);
 
-  const [colunas, setColunas] = useState<ColunasProps[]>([]) 
+  const [colunas, setColunas] = useState<ColunasProps[]>([]);
   const [NomeTemplate, setNomeTemplate] = useState("");
   const [NomeColuna, setNomeColuna] = useState("");
   const [numColunas, setNumColunas] = useState(0);
@@ -35,6 +34,8 @@ export default function CreateTemplate() {
   const [isNull, setIsNull] = useState(false);
   const [NomeColunaError, setNomeColunaError] = useState("");
   const [NomeColunaPH, setNomeColunaPH] = useState("Nome da Coluna");
+  const [indexToEdit, setIndexToEdit] = useState(-1); // Inicialmente nenhum índice está sendo editado
+  const [editedColuna, setEditedColuna] = useState<ColunasProps | null>(null); // Armazena os valores de edição
 
   function addColuna() {
     if (NomeColuna === "") {
@@ -58,19 +59,46 @@ export default function CreateTemplate() {
     setIsNull(false);
     setNomeColunaError("");
     setNomeColunaPH("Nome da Coluna");
+  }
 
+  function editarBtn(index: number) {
+    // Configura o índice da coluna que está sendo editada e os valores de edição
+    const coluna = colunas[index];
+    setIndexToEdit(index);
+    setEditedColuna(coluna);
+    setNomeColuna(coluna.nome_coluna);
+    setIsNull(coluna.nulo ? true : false);
+    setSelectValueDado(coluna.tipo_dado);
+  }
+
+  function saveEditBtn() {
+    // Verifica se algo foi editado
+    if (indexToEdit !== -1 && editedColuna) {
+      // Aplica as alterações à coluna
+      colunas[indexToEdit] = {
+        nome_coluna: NomeColuna,
+        tipo_dado: selectValueDado,
+        nulo: isNull,
+      };
+
+      // Limpa os valores de edição
+      setIndexToEdit(-1);
+      setEditedColuna(null);
+      setNomeColuna("");
+      setIsNull(false);
+      setSelectValueDado(tiposDados[0]);
+    }
   }
 
   function salvarBtn() {
-
     const NovoTemplate: TemplateProps = {
       nome: NomeTemplate,
       numero_colunas: numColunas,
       tipo_arquivo: tipo,
       limite_linhas: limiteNumber > 0 ? limiteNumber : null,
-      colunas: colunas
-    }
-    console.log(NovoTemplate)
+      colunas: colunas,
+    };
+    console.log(NovoTemplate);
   }
 
   return (
@@ -84,16 +112,16 @@ export default function CreateTemplate() {
           <div className="text-lg flex gap-3">
             <span>Nome da coluna:</span>
             <input
-                type="text"
-                value={NomeColuna}
-                onChange={(e) => {
-                    setNomeColuna("")
-                    setNomeColunaPH("Nome da coluna")
-                    setNomeColunaError("")
-                    setNomeColuna(e.target.value)
-                }}
-                className={`outline-none rounded-2xl border pl-3 w-[70%] h-8 ${NomeColunaError}`}
-                placeholder={NomeColunaPH}
+              type="text"
+              value={NomeColuna}
+              onChange={(e) => {
+                setNomeColuna("");
+                setNomeColunaPH("Nome da coluna");
+                setNomeColunaError("");
+                setNomeColuna(e.target.value);
+              }}
+              className={`outline-none rounded-2xl border pl-3 w-[70%] h-8 ${NomeColunaError}`}
+              placeholder={NomeColunaPH}
             />
           </div>
           <div className="text-lg flex justify-around">
@@ -125,66 +153,83 @@ export default function CreateTemplate() {
               </label>
             </div>
             <div className="flex justify-end items-center gap-3">
-              <button
-                className="bg-green-500 rounded-2xl border px-2 py-1 text-white font-semibold hover:bg-green-600"
-                onClick={addColuna}
-              >
-                Adicionar
-              </button>
+              {indexToEdit !== -1 ? (
+                <button
+                  className="bg-green-500 rounded-2xl border px-2 py-1 text-white font-semibold hover:bg-green-600"
+                  onClick={saveEditBtn}
+                >
+                  Salvar
+                </button>
+              ) : (
+                <button
+                  className="bg-green-500 rounded-2xl border px-2 py-1 text-white font-semibold hover:bg-green-600"
+                  onClick={addColuna}
+                >
+                  Adicionar
+                </button>
+              )}
             </div>
           </div>
         </div>
       </div>
       <div className="flex max-h-[65%] flex-col w-full h-full  items-center justify-between bg-white rounded-2xl   ">
         <div className="flex justify-between p-5 items-center w-full">
-            <div className="font-bold text-xl">Colunas do template:</div>
-            <div className="flex gap-1 justify-center items-center">
-                <span>Limite de linhas?</span>
-                <input
-                value={limiteNumber}
-                onChange={(event) => setLimiteNumber(+event.target.value)}
-                type="number"
-                className="outline-none rounded-2xl border pl-3 w-20 h-8"
+          <div className="font-bold text-xl">Colunas do template:</div>
+          <div className="flex gap-1 justify-center items-center">
+            <span>Limite de linhas?</span>
+            <input
+              value={limiteNumber}
+              onChange={(event) => setLimiteNumber(+event.target.value)}
+              type="number"
+              className="outline-none rounded-2xl border pl-3 w-20 h-8"
             />
-        </div>
-            <div className="flex gap-2 justify-center items-center">
-                <div>Tipo de Arquivo:</div>
-                <div>
-                    <select
-                        name="tipo_arquivo"
-                        id="tipo_arquivo"
-                        value={tipo}
-                        onChange={(event) => setTipo(event.target.value)}
-                        className="outline-none border rounded-2xl p-1"
-                    >
-                        {tipoArquivos.map((tipo, index) => (
-                        <option key={index} value={tipo} id={tipo}>
-                            {tipo}
-                        </option>
-                        ))}
-                    </select>
-                </div>
+          </div>
+          <div className="flex gap-2 justify-center items-center">
+            <div>Tipo de Arquivo:</div>
+            <div>
+              <select
+                name="tipo_arquivo"
+                id="tipo_arquivo"
+                value={tipo}
+                onChange={(event) => setTipo(event.target.value)}
+                className="outline-none border rounded-2xl p-1"
+              >
+                {tipoArquivos.map((tipo, index) => (
+                  <option key={index} value={tipo} id={tipo}>
+                    {tipo}
+                  </option>
+                ))}
+              </select>
             </div>
+          </div>
         </div>
         <div className="flex-1 w-full grid grid-cols-2 place-items-center overflow-y-auto gap-5 p-6 scrollbar-custom">
           {colunas.map((coluna, i) => (
             <>
               <div key={i} className="flex gap-2 justify-center items-center">
                 <div className="flex gap-2 p-1 w-full  text-white font-bold px-2 rounded-2xl bg-green-800 justify-around">
-                  <span>{i+1}.</span>
+                  <span>{i + 1}.</span>
                   <div className="flex gap-5 ">
                     <span> {coluna.nome_coluna}</span>
-                    <span className="bg-white rounded-2xl text-black px-2 flex gap-2 justify-center items-center"> {coluna.nulo ? "Nulo" : "Não Nulo"}
-                    <div className="p-1 w-1 h-1 rounded-full bg-yellow-500"></div>
+                    <span className="bg-white rounded-2xl text-black px-2 flex gap-2 justify-center items-center">
+                      {" "}
+                      {coluna.nulo ? "Nulo" : "Não Nulo"}
+                      <div className="p-1 w-1 h-1 rounded-full bg-yellow-500"></div>
                     </span>
-                    <span className="bg-white rounded-2xl text-black px-2 flex gap-2 justify-center items-center"> {coluna.tipo_dado}
-                    <div className="p-1 w-1 h-1 rounded-full bg-yellow-500"></div>
+                    <span className="bg-white rounded-2xl text-black px-2 flex gap-2 justify-center items-center">
+                      {" "}
+                      {coluna.tipo_dado}
+                      <div className="p-1 w-1 h-1 rounded-full bg-yellow-500"></div>
                     </span>
                   </div>
                 </div>
                 <ul className="flex gap-2">
-                  <li title="Editar coluna"><Pencil className="w-7 h-7 cursor-pointer"/> </li>
-                  <li title="excluir coluna"><Trash className="w-7 h-7 cursor-pointer"/> </li>
+                  <li title="Editar coluna">
+                    <Pencil onClick={() => editarBtn(i)} className="w-7 h-7 cursor-pointer" />{" "}
+                  </li>
+                  <li title="excluir coluna">
+                    <Trash className="w-7 h-7 cursor-pointer" />{" "}
+                  </li>
                 </ul>
               </div>
             </>
