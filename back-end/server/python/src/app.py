471d435f997c39cd11aad32b44a31d4f724a1910
Matheus Souza Soares <980189@queroquero.local>
2023-10-15 19:53:57 -0300

    verificacao se a extensao do arquivo bate com a do template

--- back-end/server/python/src/app.py
+++ back-end/server/python/src/app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pandas as pd
@@ -10,15 +11,29 @@ def upload_file():
     
     user_id = request.form.get('user_id')
     template = request.form.get('template')
-    print(user_id, template, request.files['file'])
+    campos = request.form.get('campos')
+    print(user_id, "\n", template, "\n",campos, "\n",request.files['file'])
     
     if user_id is None or template is None:
         return jsonify({'error': 'Dados incompletos no corpo da solicitação'}), 400
     
     if 'file' not in request.files:
         return 'Nenhum arquivo foi enviado', 400
+    if not template:
+        return jsonify({'error': 'O campo de template é obrigatório'}), 400
 
+    try:
+        template_data = json.loads(template)
+    except json.JSONDecodeError:
+        return jsonify({'error': 'O campo de template não é um JSON válido'}), 400
+    
     file = request.files['file']
+    
+    file_extension = file.filename.split('.')[-1].lower()
+
+    if file_extension != template_data.get('formato', '').lower():
+        return jsonify({'error': 'A extensão do arquivo não corresponde ao formato do template'}), 400
+
 
     if file.filename == '':
         return 'Nome de arquivo vazio', 400
