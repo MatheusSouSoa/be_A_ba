8e3b0c5ac1b2a423206b8f9d4abf2e2b8fc8d000
Matheus Souza Soares <980189@queroquero.local>
2023-10-15 20:34:05 -0300

    salvando arquivo no servidor e inicio db

--- back-end/server/python/src/app.py
+++ back-end/server/python/src/app.py
@@ -2,9 +2,26 @@ import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pandas as pd
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
+
+def validate_fields(file, template_data):
+    df = pd.read_json(file)
+    file_fields = df.columns.tolist()
+
+    template_fields = [campo['nome'] for campo in template_data['data']]
+
+    if len(file_fields) != len(template_fields):
+        return False
+
+    for campo in file_fields:
+        if campo not in template_fields:
+            return False
+
+    return True
 
 @app.route('/api/files/upload', methods=['POST'])
 def upload_file():
@@ -41,14 +58,29 @@ def upload_file():
     if file:
         if file.filename.endswith('.csv'):
             df = pd.read_csv(file)
-
         elif file.filename.endswith('.xlsx'):
             df = pd.read_excel(file)
-
         else:
             return 'Formato de arquivo não suportado', 400
+        
+        user_directory = os.path.join('data', 'users', user_id, 'files')
+        if not os.path.exists(user_directory):
+            os.makedirs(user_directory)
 
-        return df.to_json()
+        current_time = datetime.now()
+        timestamp = current_time.strftime('%Y%m%d%H%M%S')
+        filename = f'uploaded_file_{timestamp}.csv'  
 
+        file_path = os.path.join(user_directory, filename)
+
+        file.save(file_path)
+        download_path = f'/api/files/download/{user_id}/{filename}'
+
+        response_data = {
+            'download_path': download_path,
+            'dataframe': df.to_json()
+        }
+        return jsonify(response_data)
+    
 if __name__ == '__main__':
     app.run(debug=True)
